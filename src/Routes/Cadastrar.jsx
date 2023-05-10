b4d324bf94b353bd3cf6104828805b143abd344e
yujiarima17 <arimayuji2@gmail.com>
2023-05-10 09:06:12 -0300

    modal termos de uso

--- src/Routes/Cadastrar.jsx
+++ src/Routes/Cadastrar.jsx
@@ -6,7 +6,59 @@ import { useState } from "react";
 import "./Cadastro.css";
 import { Link } from "react-router-dom";
 import { yupResolver } from "@hookform/resolvers/yup";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      id="modal"
+    >
+      <Modal.Header >
+        <Modal.Title id="contained-modal-title-vcenter">
+          Termos de Uso
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body centered>
+        <p>
+          Estes são os termos e condições de uso do site ToSalvo localizado. Ao
+          acessar este site, você concorda com os termos e condições descritos
+          abaixo. O site utiliza cookies para personalizar sua experiência na
+          internet e, ao acessá-lo, você concorda com o uso dos cookies
+          necessários.{" "}
+        </p>
+        <p>
+          Os cookies são arquivos de texto colocados em seu disco rígido por um
+          servidor de página da web. O site pode utilizar cookies para coletar,
+          armazenar ou rastrear informações para fins estatísticos e de
+          marketing. Você tem a opção de aceitar ou rejeitar cookies opcionais,
+          mas alguns cookies obrigatórios são necessários para a operação do
+          site e aceitá-los significa também aceitar cookies de terceiros.{" "}
+        </p>
+        <p>
+          ToSalvo e seus licenciadores têm os direitos de propriedade
+          intelectual de todos os materiais postados no ToSalvo. Você não tem
+          permissão para copiar, vender, alugar, sublocar, reproduzir, duplicar,
+          copiar ou redistribuir os materiais do ToSalvo.
+        </p>
+        <p>
+          Certas organizações,como agências governamentais, mecanismos de busca,
+          organizações demídia e empresas credenciadas, podem criar links para o
+          site sem a aprovação prévia por escrito. No entanto, o link não deve
+          ser enganoso e falsamente implicar qualquer patrocínio, endosso ou
+          aprovação pelo ToSalvo.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <button onClick={props.onHide}>Close</button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 const cadastrar = () => {
   const {
     register,
@@ -26,6 +78,7 @@ const cadastrar = () => {
   };
   const [showSenha, setSenha] = useState("bi bi-eye-slash");
   const [showInputType, setInputType] = useState("password");
+  const [modalShow, setModalShow] = useState(false);
 
   const handleToggleSenha = () => {
     setSenha((prevState) =>
@@ -83,11 +136,11 @@ const cadastrar = () => {
           <button type="submit">Cadastrar</button>
           <div className="opcoes">
             <ul>
-              <li>
-                <Link to="/Termos de Uso" className="link">
-                  Termos de Uso
-                </Link>
-              </li>
+              <li onClick={() => setModalShow(true)}>Termos de Uso</li>
+              <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+              />
             </ul>
           </div>
         </form>
