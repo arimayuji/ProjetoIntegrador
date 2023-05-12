59176c9bb33082bf7594879952ab1d8f11308098
cadub <cadubas1@gmail.com>
2023-05-12 08:49:33 -0300

    Adicionando a Função de Verificar Histórico de Notas por Matéria

--- src/db/BancoDeDados.jsx
+++ src/db/BancoDeDados.jsx
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { collection, deleteDoc, doc, getDocs, getFirestore, setDoc } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs, getFirestore, setDoc, onSnapshot } from 'firebase/firestore';
 import { useEffect, useState } from 'react'
-import { useNavigate } from "react-router-dom";
+
 
 //dados do banco firebase
 const firebaseapp = initializeApp({
@@ -32,4 +32,39 @@ export const ListaUsuarios = () => {
     },[])
 
     return users
+}
+
+export const VerificaHistorico = (email, materia) => {
+
+    const [notas, setNotas] = useState([]);
+
+    useEffect(() => {
+
+        const parentDocRef = doc(useCollectionRef, email);
+        const subCollectionRef = collection(parentDocRef, materia);
+
+        // adiciona listener para a subcoleção
+        const unsubscribe = onSnapshot(subCollectionRef, (subCollectionSnapshot) => {
+            const subCollectionDocs = subCollectionSnapshot.docs;
+            
+            setNotas(
+                subCollectionDocs
+                    .filter(doc => doc.data().P1 !== undefined)
+                    .map(doc => {
+                        const data = doc.data();
+                        return {
+                            id: doc.id,
+                            P1: data.P1,
+                        };
+                    })
+            );
+        });
+
+        // retorna função de cleanup para remover o listener quando o componente for desmontado
+        return () => unsubscribe();
+    },[])
+    
+    if(notas.length !== 0) {
+        return notas;
+    }
 }
