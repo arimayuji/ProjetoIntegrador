1c4a584cf1858a736089c9fe6d0028e74f22c751
Leonardo Ribeiro De Almeida <Leozinhooalmeida@hotmail.com>
2023-05-12 09:14:52 -0300

    Imagens Quase Todas Atualizadas + Links das redes Sociais

--- src/Routes/SobreNos.jsx
+++ src/Routes/SobreNos.jsx
@@ -29,9 +29,9 @@ const Layout = () => {
               <h3>Leonardo</h3>
             </div>
             <div className="icons">
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-github"></i>
-              <i class="bi bi-instagram"></i>
+              <a href="https://www.linkedin.com/in/leonardo-ribeiro-27509a271/"><i className="bi bi-linkedin"></i></a>
+              <a href="https://github.com/TheRibeiro"><i className="bi bi-github"></i></a>
+              <a href="https://www.instagram.com/ribeiro_le0/"><i className="bi bi-instagram"></i></a>
             </div>
 
           </div>
@@ -43,9 +43,9 @@ const Layout = () => {
             </div>
 
             <div className="icons">
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-github"></i>
-              <i class="bi bi-instagram"></i>
+              <a href="https://www.linkedin.com/in/yuji-arima-7b7059209/"><i className="bi bi-linkedin"></i></a>
+              <a href="https://github.com/yujiarima17"><i className="bi bi-github"></i></a>
+              <a href="https://www.instagram.com/_yarim_17/"><i className="bi bi-instagram"></i></a>
             </div>
           </div>
           <div className="member">
@@ -56,9 +56,9 @@ const Layout = () => {
             </div>
 
             <div className="icons">
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-github"></i>
-              <i class="bi bi-instagram"></i>
+              <a href="https://www.linkedin.com/in/luigi-ishii-055136236/"><i className="bi bi-linkedin"></i></a>
+              <a href="https://github.com/luigishii"><i className="bi bi-github"></i></a>
+              <a href="https://www.instagram.com/luigishii/"><i className="bi bi-instagram"></i></a>
             </div>
           </div>
         </div>
@@ -71,22 +71,25 @@ const Layout = () => {
             </div>
 
             <div className="icons">
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-github"></i>
-              <i class="bi bi-instagram"></i>
+              <a href="https://www.linkedin.com/in/henrique-yui-oda-279847212/"><i className="bi bi-linkedin"></i></a>
+              <a href="https://github.com/henriqueoda"><i className="bi bi-github"></i></a>
+              <a href="https://www.instagram.com/henriqueoda7/  "><i className="bi bi-instagram"></i></a>
             </div>
           </div>
           <div className="member">
             <img src={Cadu} alt="Membro 5" />
             <div className="bio">
-              <i class="bi bi-person-bounding-box"> </i>
-              <h3>Cadu</h3>
+       
+                <i class="bi bi-person-bounding-box"> </i>
+                <h3>Cadu</h3>
+                
+             
             </div>
 
             <div className="icons">
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-github"></i>
-              <i class="bi bi-instagram"></i>
+              <a href="https://www.linkedin.com/in/cadu-dias-45b596238/"><i className="bi bi-linkedin"></i></a>
+              <a href="https://github.com/Cadub"><i className="bi bi-github"></i></a>
+              <a href="https://www.instagram.com/dias.cadu/"><i className="bi bi-instagram"></i></a>
             </div>
           </div>
           <div className="member">
@@ -97,9 +100,9 @@ const Layout = () => {
             </div>
 
             <div className="icons">
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-github"></i>
-              <i class="bi bi-instagram"></i>
+              <a href="https://www.linkedin.com/in/rafael-carrasco-66b7581a3/"><i className="bi bi-linkedin"></i></a>
+              <a href="https://github.com/RafCarrasco"><i className="bi bi-github"></i></a>
+              <a href="https://www.instagram.com/orafaelcarrasco/"><i className="bi bi-instagram"></i></a>
             </div>
           </div>
         </div>
