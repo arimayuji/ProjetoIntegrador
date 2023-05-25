fbe25427b9a60f0e94704967151e340e9929b1b0
yuji <arimayuji2@gmail.com>
2023-05-25 19:40:38 -0300

    melhoria da homepage + carousel

--- src/Layout/CursosLayout.jsx
+++ src/Layout/CursosLayout.jsx
@@ -1,7 +1,4 @@
-import NavBar from "../Component/navBar";
-import Footer from "../Component/Footer";
 import "./Layout.css";
-import { GlobalStyle } from "../GlobalStyle";
 import { Outlet } from "react-router-dom";
 const CursosLayout = () => {
   return (
--- src/Routes/Homepage.jsx
+++ src/Routes/Homepage.jsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logoToSalvo from "../images/logo-toSalvoBlue.png";
 import "./Homepage.css";
-import office from "../images/office365.png";
 import "./Office.css";
+import Carousel from 'react-bootstrap/Carousel';
+
 
 function Layout() {
   const [pageLoaded, setPageLoaded] = useState(false);
@@ -20,9 +21,37 @@ function Layout() {
     <>
       <div className={`tela ${pageLoaded ? "show" : "loading"}`}>
         <img src={logoToSalvo} alt="" />
-        
-        <p> Bem-vindo ao Tô Salvo, o seu site de calculadora de notas universitárias!</p>
+        <section className="texto-1">
+          <p> Bem-vindo ao Tô Salvo, o seu site de calculadora de notas universitárias!</p>
           <p >Experimente agora mesmo e comece a usar o Tô Salvo para seu sucesso acadêmico!</p>
+        </section>
+
+        <Carousel interval={3000} controls={false} indicators={false}
+          className="custom-carousel" transition="fade">
+          <Carousel.Item>
+            <div className="carousel-conteudo"><p>Calcule suas notas universitárias de forma rápida !</p></div>
+
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="carousel-conteudo"><p>Use a Calculadora Tô Salvo para se planejar!</p></div>
+
+
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="carousel-conteudo"><p>Acompanhe seu desempenho acadêmico conosco!</p></div>
+
+
+          </Carousel.Item>
+        </Carousel>
+        
+        <section className="texto-2">
+          <p>
+            Com o Tô Salvo, você terá o controle das suas notas e poderá planejar seu sucesso acadêmico de forma eficiente.
+
+          </p>
+          <p> Experimente agora mesmo e aproveite todos os recursos que nossa calculadora de notas universitárias tem a oferecer!</p>
+        </section>
+
         <div className="office">
           <Link to="/Login" className="link">
             <button>Faça Login</button>
