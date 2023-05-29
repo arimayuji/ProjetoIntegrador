d08d945c300dc1f69af4b45a3e487893bfc98d6e
yuji <arimayuji2@gmail.com>
2023-05-29 17:33:32 -0300

    mudanca do title e logos do header

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,7 +3,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
 import { GlobalStyle } from "../GlobalStyle";
-import logo from "../images/toSalvo-Branco.png"
+import logo from "../images/logo-maua.png"
 const Footer = () => {
   return (
     <>
--- src/Component/HomeNavBar.jsx
+++ src/Component/HomeNavBar.jsx
@@ -1,4 +1,4 @@
-import logo from "../images/logo-maua.png";
+import logo from "../images/toSalvo-Branco.png";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
